fa129502acaf604d95bd26e38b9d5707f5c27b9b
Naoya Kanai <naopon@gmail.com>
2014-05-09 22:47:54 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kaohon::Application.config.secret_key_base = '0699b1de92506fa416fd1b51debbe4f69a341ad641d024ad07d3105e9456f60e4509a6664dd7a76bf87c4d8a7772374d62ca7a42ca0014c47fb2bcb3653d06ea'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kaohon::Application.config.session_store :cookie_store, key: '_kaohon_session'
